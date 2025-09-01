19ed62359e9693ae10abf30fcddd404e241364af
Kotaro Miyauchi <j324404@ns.kogakuin.ac.jp>
2025-09-01 13:54:24 +0900

    Add progress bar animation

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 window.addEventListener("load", function() {
     document.querySelector("#loading").style.display = "none";
 });
+
+window.addEventListener("DOMContentLoaded", () => {
+    const progresses = document.querySelectorAll("#progressBar");
+
+    progresses.forEach(progressBar => {
+        let current = 0;
+        const target = progressBar.value;
+        const speed = 40;
+
+        const interval = setInterval(() => {
+            if (current < target) {
+            current++;
+            progressBar.value = current;
+            } else {
+            clearInterval(interval); 
+            }
+        }, speed);
+    });
+});
